c7ce1b94197361a822161b37cbaa575ab577b5cb
AndrewShepetchuk0 <109487801+AndrewShepetchuk0@users.noreply.github.com>
2023-04-05 01:47:18 +0000

    Update Exercise5.java

--- variablesexercises/Exercise5.java
+++ variablesexercises/Exercise5.java
@@ -5,19 +5,28 @@ import java.util.Scanner;
 public class Exercise5 {
 
     public static void main(String[] args) {
+        //Java scanner initialization 
         Scanner input = new Scanner (System.in);
+        
+        //Variables
         int containerNumber;
         double refund, containerSize;
+        
+        //User input
         System.out.print("How many containers do you have?");
         containerNumber = input.nextInt();
         System.out.print("Whats the size of tour container(s) in liters");
         containerSize = input.nextDouble();
+        
+        //Refund Calculation
         if (containerSize <= 1){
             refund = containerNumber * 0.1;
         }
         else{
             refund = containerNumber * 0.25;
         }
+        
+        //Refund display
         System.out.println("Your refund is $" + refund);
     }
     
